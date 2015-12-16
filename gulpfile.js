a5cfbd91f9a57e63dab6df429b9bbebea39f79e7
Guillaume Royer <perso@guilro.com>
2015-12-16 12:43:25 +0100

    add source maps to css

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ var changed = require('gulp-changed');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function() {
   gulp.src('src/styles/theme.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write('.'))
     .pipe(changed('dist/theme'))
     .pipe(gulp.dest('dist/theme'))
   ;
