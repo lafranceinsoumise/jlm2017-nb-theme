634e240e8202c9611188a0c1d935fb850b811b6e
Guillaume Royer <perso@guilro.com>
2016-03-24 22:29:53 +0100

    fix #2 youhou !

--- src/javascripts/components.js
+++ src/javascripts/components.js
@@ -11,3 +11,25 @@ $('.collapse').on('hide.bs.collapse', function (event) {
                          '[data-toggle="collapse"][data-target="#' + event.target.id + '"]');
   trigger.find('.fa-arrow-circle-o-down').removeClass('fa-arrow-circle-o-down').addClass('fa-arrow-circle-o-right');
 });
+
+$('.ajaxForm').on('submit', function(event) {
+  $form = $(this);
+  $form.find('input[type="submit"]').val= "Chargement...";
+  $form.find('input[type="submit"]').prop('disabled', true);
+  console.log('disabled');
+
+  var dispatch = window.NB.EventHub.dispatch;
+  dispatch.bind(window.NB.EventHub);
+
+  window.NB.EventHub.dispatch = function(e) {
+    if (e == 'form.success') {
+        // success
+    } else if (e == 'form.failure') {
+      $form.find('input[type="submit"]').prop('disabled', false);
+    }
+
+    console.log(e);
+
+    return dispatch.apply(this, arguments);
+  };
+});
